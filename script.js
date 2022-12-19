47e41f2e7f02452e7eacd62995aab39e4e38c559
samuelgmar <79518621+samuelgmar@users.noreply.github.com>
2022-12-19 02:06:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 function onScanSuccess(decodedText, decodedResult) {
+    console.log(`Code scanned = ${decodedText}`, decodedResult);
     const element = $("#code");
     const textToReplace = element.text();
     const newText = decodedResult.decodedText;
+    $("#qr-shaded-region").style("border-width","199px 78px");
     element.text(newText); 
-    console.log(`Code scanned = ${decodedText}`, decodedResult);
 }
 var html5QrcodeScanner = new Html5QrcodeScanner(
-	"qr-reader", { fps: 10 });
+	"qr-reader", { fps: 10, qrbox: 400 });
 html5QrcodeScanner.render(onScanSuccess);
+
